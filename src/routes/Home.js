c604cc9e51a048b469668076126aa2ea8ff3a4b8
HyeYoungee <137126899+HyeYoungee@users.noreply.github.com>
2023-10-12 11:42:57 +0900

    post 입력 기능 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../firebase";
+import { collection, addDoc, serverTimestamp } from "firebase/firestore"; 
 
-const Home = () => <h2>Home Page</h2>
+const Home = () => {
+  const [post, setPost] = useState('');
+  const onChange = (e) => {
+    // const val = e.target.value; //ECMA script 2015이전 문법
+    const {target:{value}} = e; //ES6 버전
+    setPost(value);
+  }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const docRef = await addDoc(collection(db, "posts"), {
+      content: post,
+      date: serverTimestamp()
+    });
+    console.log("Document written with ID: ", docRef.id);
+  }
+  console.log(post);
+  return(
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={post} placeholder="포스트 쓰기" onChange={onChange}/>
+        <input type="submit" value="입력"/>
+      </form>
+    </div>
+  )
+}
 
 export default Home;
